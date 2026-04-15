4cc683dad7f45e6924bd1dbeba45e1b957ac93a0
Kasinathan A B <kasia15b6@gmail.com>
2026-04-16 01:32:50 +0530

    Implement containsDuplicate function using unordered_set

---
+++ containsDuplicate.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        unordered_set<int> st;
+        int n=nums.size();
+        for(int i=0;i<n;i++){
+           if(st.count(nums[i])){
+            return true;
+            }else{
+                st.insert(nums[i]);
+            }
+        }
+        return false;
+    }
+};
