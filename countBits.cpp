3dc1a1486787bfb4464fb43da1119b3eda2afb18
Kasinathan A B <kasia15b6@gmail.com>
2026-04-27 23:34:50 +0530

    Implement countBits function in Solution class

---
+++ countBits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> cclass Solution {
+public:
+    vector<int> countBits(int n) {
+        vector<int> ans(n+1);
+        for(int i=0;i<=n;i++){
+           ans[i]= __builtin_popcount(i);
+        }
+        return ans;
+    }
+};(int n) {
+        vector<int> ans(n+1);
+        for(int i=0;i<=n;i++){
+           ans[i]= __builtin_popcount(i);
+        }
+        return ans;
+    }
+};
