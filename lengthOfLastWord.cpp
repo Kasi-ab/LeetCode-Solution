b5eba1530c60597dfd00434dbf2f661246a95490
Kasinathan A B <kasia15b6@gmail.com>
2026-05-09 23:01:01 +0530

    Add lengthOfLastWord function
    Implement function to find length of last word in a string.

---
+++ lengthOfLastWord.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+   int lengthOfLastWord(string s) {
+      int n=s.length()-1;
+      int count=0;
+      for(int i=n;i>=0;i--){
+         if(s[i]==' '&& count>0){
+           return count;
+         }
+         if(s[i]==' '&& count==0){
+           continue;
+         }
+         count++;
+      }
+        return count;
+    }
+};
